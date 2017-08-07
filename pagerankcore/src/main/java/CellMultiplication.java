4d113d5b0a2bbbcfd3ef941fe3510fd4b2db4fc8
zanydruid <zoom198852800>
2017-08-07 17:17:17 -0400

    finished two mappers in first MR.

--- pagerankcore/src/main/java/CellMultiplication.java
+++ pagerankcore/src/main/java/CellMultiplication.java
@@ -5,13 +5,20 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class CellMutiplication {
+public class CellMultiplication {
 
     public static class TransiitionMapper extends Mapper<Object, Text, Text, Text> {
 
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            // input: 1   2,3,5,7,12
+            // output: <1, 2=1/5>, <1, 3=1/5>, <1, 5=1/5>...
+            String fId = value.toString().split("\t")[0];
+            String[] tos = value.toString().split("\t")[1].split(",");
 
+            for(String to : tos) {
+                context.write(new Text(fId), new Text(to + "=" + (double)1/tos.length));
+            }
         }
     }
 
@@ -19,7 +26,11 @@ public class CellMutiplication {
 
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            // input: 1\t1 / 2\t1 / 3\t1...
+            // output: 1 1, 2 1, 3 1...
 
+            String[] words = value.toString().split("\t");
+            context.write(new Text(words[0]), new Text(words[1]));
         }
     }
 
